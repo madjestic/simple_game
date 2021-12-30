9cfff6953c117d78884f654daeef7d52682ef60c
madjestic <madjestic13@gmail.com>
2021-12-30 01:13:28 +0100

    adding imports, print formatting

--- GameState/Game.hs
+++ GameState/Game.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module GameState.Game where
 
 import Control.Monad.State as State
+import Control.Monad.Reader
+import Control.Monad.Writer
+
 import SDL.Time (delay)
 
 data GameState =
@@ -13,11 +18,15 @@ initState :: StateT GameState IO ()
 initState = do
   return ()
 
+incRSW :: (MonadReader Integer m, MonadState GameState m, MonadWriter [Integer] m) => m ()
+--incRSW = ask >>= modify . flip inc >> get >>= tell . (:[]) . tick
+incRSW = get >>= tell . (:[]) . tick
+
 loop :: StateT GameState IO GameState
 loop = do
   liftIO $ delay 10
-  liftIO $ print  $ "Awaiting user input:"
-  liftIO $ getLine >>= print
+  liftIO $ print "Awaiting user input:"
+  liftIO $ getLine >>= (\x -> putStrLn $ "It's :" ++ show x)
   loop
 
 startState :: GameState
